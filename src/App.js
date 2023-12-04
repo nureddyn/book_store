0c11b3b52800709d441703334ca25b009f4c26bd
nureddyn <nureddynballout@gmail.com>
2023-12-04 18:38:59 -0300

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+// import Home from "./pages/Home";
+// import SearchBooks from "./pages/SearchBooks";
+
 import Home from "./components/Home";
 import SearchBooks from "./components/SearchBooks";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <SearchBooks />
+      {/* Create Header Nav here */}
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/search" element={<SearchBooks />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="header">Header</div>
+    <div className="header">
+      <Link to="/">
+        <div>Home</div>
+      </Link>
+      <Link to="/search">
+        <div>Search Books</div>
+      </Link>
+    </div>
   )
 }
